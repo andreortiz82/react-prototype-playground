9d1d42d8ed00e03931705c1690e56ab6b6188d11
Andre Ortiz <andreortiz82@gmail.com>
2018-09-09 09:50:30 -0500

    basic routing added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
+import Cart from './components/Cart'
+import Dragging from './components/Dragging'
+import Login from './components/Login'
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div>
+          <Link to="/">Login</Link>
+          <Link to="/dragging">Dragging</Link>
+          <Link to="/cart">Cart</Link>
+          <hr />
+          <Route exact path="/" component={Login} />
+          <Route path="/dragging" component={Dragging} />
+          <Route path="/cart" component={Cart} />
+        </div>
+      </Router>
+    )
   }
 }
-
-export default App;
---
+++ src/components/Dragging.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Dragging extends Component {
+  render() {
+    return (
+      <div className="dragging-component">
+        Dragging and Droppings
+      </div>
+    );
+  }
+}
